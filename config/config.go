14d6d4f27453bb37e3bff357acbb9340a321818b
ShrutiKhanduja <shrutikhanduja3859@gmail.com>
2021-07-11 22:04:00 +0530

    Added db adapter

--- config/config.go
+++ config/config.go
@@ -8,7 +8,16 @@ import (
 )
 
 type Conf struct {
+	Debug  bool `env:"DEBUG,default=true"`
+	Db     dbConf
 	Server serverConf
+}
+type dbConf struct {
+	Host     string `env:"DB_HOST,default=db"`
+	Port     int    `env:"DB_PORT,default=3306"`
+	Username string `env:"DB_USER,default=myapp_user"`
+	Password string `env:"DB_PASS,myapp_pass"`
+	DbName   string `env:"DB_NAME,myapp_db"`
 }
 
 type serverConf struct {
